49604aa799d69db760b40bff6d1901154b9a51e1
Matt DeMartino <matt.s.demartino@gmail.com>
2026-01-07 15:56:50 -0500

    Create MetSingleArtwork struct

--- server/museums/met.go
+++ server/museums/met.go
@@ -1,1 +1,12 @@
 package museums
+
+type MetSingleArtwork struct {
+	ObjectID          int    `json:"objectID"`
+	ObjectName        string `json:"objectName"`
+	ArtistDisplayName string `json:"artistDisplayName"`
+	ObjectDate        string `json:"objectDate"`
+	Medium            string `json:"medium"`
+	Repository        string `json:"repository"`
+	PrimaryImage      string `json:"primaryImage"`
+	PrimaryImageSmall string `json:"primaryImageSmall"`
+}
