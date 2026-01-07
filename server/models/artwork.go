3827a436f0c893404b30984c17b5f8025d579278
Matt DeMartino <matt.s.demartino@gmail.com>
2026-01-07 15:49:19 -0500

    Create base SingleArtwork struct

---
+++ server/models/artwork.go
@@ -0,0 +1,11 @@
+package models
+
+type SingleArtwork struct {
+	ID           int
+	ArtworkTitle string
+	ArtistName   string
+	DateCreated  string
+	ArtMedium    string
+	ImageLarge   string
+	ImageSmall   string
+}
