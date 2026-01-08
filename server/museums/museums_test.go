3a9cc8a0912803a1bb9a350085ac9ce63fc60ec4
Matt DeMartino <matt.s.demartino@gmail.com>
2026-01-08 11:43:55 -0500

    Testing for normalizing harvard artwork response

--- server/museums/museums_test.go
+++ server/museums/museums_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestNormalizeArtwork(t *testing.T) {
+func TestNormalizeMetArtwork(t *testing.T) {
 	raw := MetSingleArtwork{
 		ObjectID:          436105,
 		ObjectName:        "The Death of Socrates",
@@ -24,5 +24,32 @@ func TestNormalizeArtwork(t *testing.T) {
 
 	if normalized.ArtworkTitle != "The Death of Socrates" {
 		t.Errorf("Expected title The Death of Socrates, returned %s", normalized.ArtworkTitle)
+	}
+}
+func TestNormalizeHarvardArtwork(t *testing.T) {
+	raw := HarvardSingleArtwork{
+		ID:     230120,
+		Dated:  "c. 1884-1885",
+		Medium: "Pot metal, opalescent, and uncolored glass with vitreous paint",
+		People: struct {
+			DisplayName string "json:\"displayname\""
+		}{
+			DisplayName: "John La Farge",
+		},
+		Primaryimageurl: "https://nrs.harvard.edu/urn-3:HUAM:38659_dynmc",
+		Title:           "When the Morning Stars Sang Together and All the Sons of God Shouted for Joy",
+	}
+	client := NewHarvardClient("test")
+	normalized := client.NormalizeArtwork(raw)
+
+	if normalized.ID != 230120 {
+		t.Errorf("Expected ID 230120, returned ID %d", normalized.ID)
+	}
+
+	if normalized.ArtworkTitle != "When the Morning Stars Sang Together and All the Sons of God Shouted for Joy" {
+		t.Errorf("Expected title When the Morning Stars Sang Together and All the Sons of God Shouted for Joy, returned %s", normalized.ArtworkTitle)
+	}
+	if normalized.ArtistName != "John La Farge" {
+		t.Errorf("Expected artist name John La Farge, returned %s", normalized.ArtistName)
 	}
 }
