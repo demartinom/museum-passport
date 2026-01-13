e3f6746162f4c5a03cf04996d26f3efcd4e953ad
Matt DeMartino <matt.s.demartino@gmail.com>
2026-01-13 16:35:00 -0500

    Init cache and functions for setting and getting

---
+++ server/cache/cache.go
@@ -0,0 +1,40 @@
+package cache
+
+import (
+	"sync"
+
+	"github.com/demartinom/museum-passport/models"
+)
+
+type Cache struct {
+	Data map[string]CachedItem
+	mu   sync.RWMutex
+}
+
+type CachedItem struct {
+	Artwork models.SingleArtwork
+}
+
+func NewCache() *Cache {
+	return &Cache{Data: make(map[string]CachedItem)}
+}
+
+func (c *Cache) SetArtwork(id string, artwork models.SingleArtwork) {
+	c.mu.Lock()
+	defer c.mu.Lock()
+
+	c.Data[id] = CachedItem{
+		Artwork: artwork,
+	}
+}
+
+func (c *Cache) GetArtwork(id string) (models.SingleArtwork, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	artwork, exists := c.Data[id]
+	if !exists {
+		return models.SingleArtwork{}, false
+	}
+	return artwork.Artwork, true
+}
