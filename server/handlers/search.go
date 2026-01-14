9901f0f1a4e1300babef4fa264b815d3e42cd902
Matt DeMartino <matt.s.demartino@gmail.com>
2026-01-14 16:29:23 -0500

    Implement search by name feature

---
+++ server/handlers/search.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/demartinom/museum-passport/museums"
+)
+
+type SearchHandler struct {
+	Clients map[string]museums.Client
+}
+
+func NewSearchHandler(clients map[string]museums.Client) *SearchHandler {
+	return &SearchHandler{Clients: clients}
+}
+
+func (s *SearchHandler) SearchArtwork(w http.ResponseWriter, r *http.Request) {
+	museum := r.URL.Query().Get("museum")
+	name := r.URL.Query().Get("name")
+
+	artwork, err := s.Clients[museum].Search(museums.SearchParams{Name: name})
+	if err != nil {
+		fmt.Println("Error:", err)
+		http.Error(w, "Invalid search", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(artwork)
+}
