21842c840ab9e65992614850d9b43ecf5096435f
Matt DeMartino <matt.s.demartino@gmail.com>
2026-02-13 15:59:11 -0500

    Begin search page

--- client/app/search/page.tsx
+++ client/app/search/page.tsx
@@ -1,7 +1,35 @@
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 const Search = () => {
   return (
     <div>
-      <form></form>
+      <form className="w-1/4">
+        {/* Combobox instead? */}
+        <div className="flex">
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Field" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="art">Artwork</SelectItem>
+                <SelectItem value="artist">Artist</SelectItem>
+                <SelectItem value="medium">Medium</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+
+          <Input type="text" id="input-search"></Input>
+        </div>
+      </form>
     </div>
   );
 };
