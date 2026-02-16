18837cc4c12c665d7d73295cd00aa8a87fe79d8b
Matt DeMartino <matt.s.demartino@gmail.com>
2026-02-16 16:16:07 -0500

    Create grid for displaying art search results

--- client/app/search/page.tsx
+++ client/app/search/page.tsx
@@ -56,6 +56,21 @@ const Search = () => {
           </Button>
         </div>
       </form>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        {results?.Art.map((item) => (
+          <div
+            key={item.ID}
+            className="relative aspect-square overflow-hidden rounded-lg"
+          >
+            <Image
+              src={item.ImageSmall}
+              alt={item.ArtworkTitle}
+              fill
+              className="object-contain"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
