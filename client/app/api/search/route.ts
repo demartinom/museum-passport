07299edef733e98e806a1465e09b9a6cf9ed1d06
Matt DeMartino <matt.s.demartino@gmail.com>
2026-04-06 15:30:05 -0400

    Remove base url variable

--- client/app/api/search/route.ts
+++ client/app/api/search/route.ts
@@ -1,12 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const baseUrl = process.env.API_URL;
-
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const query = searchParams.toString();
 
-  const res = await fetch(`${baseUrl}/api/search?${query}`);
+  const res = await fetch(`${process.env.API_URL}/api/search?${query}`);
   const data = await res.json();
 
   return NextResponse.json(data);
