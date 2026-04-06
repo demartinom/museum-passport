60e68f1192a3dc36b7aa86deb67c4ca3952d036b
Matt DeMartino <matt.s.demartino@gmail.com>
2026-04-06 15:30:20 -0400

    Update api call to use correct api url

--- client/app/art/[id]/page.tsx
+++ client/app/art/[id]/page.tsx
@@ -11,9 +11,7 @@ const SingleArtwork = async ({
 
   let data: Art;
   try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/artwork/${id}`,
-    );
+    const res = await fetch(`${process.env.API_URL}/api/artwork/${id}`);
 
     data = await res.json();
   } catch (err) {
@@ -55,7 +53,6 @@ const SingleArtwork = async ({
               fill
               alt={data.ArtworkTitle}
               className="object-contain"
-              unoptimized
             />
           </div>
         </div>
