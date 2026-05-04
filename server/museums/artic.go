a5b7abb1db19e858a39a95bce7b024255617ea0d
Matt DeMartino <matt.s.demartino@gmail.com>
2026-05-04 14:46:28 -0400

    Create single artwork struct for Artic

--- server/museums/artic.go
+++ server/museums/artic.go
@@ -10,3 +10,13 @@ type ArticClient struct {
 func NewArticClient(cache *cache.Cache) *ArticClient {
 	return &ArticClient{BaseURL: "https://api.artic.edu/api/v1/artworks", Cache: cache}
 }
+
+type ArticSingleArtwork struct {
+	ID           int    `json:"id"`
+	DateStart    int    `json:"date_start"`
+	Medium       string `json:"medium_display"`
+	Artist       string `json:"artist_title"`
+	ImageID      string `json:"image_id"`
+	Title        string `json:"title"`
+	PublicDomain bool   `json:"is_public_domain"`
+}
