d36a42af6a294afbbc981d8e3232fbcbf599b24d
Matt DeMartino <matt.s.demartino@gmail.com>
2026-05-06 14:03:12 -0400

    Fix tests

--- server/handlers/artwork_test.go
+++ server/handlers/artwork_test.go
@@ -9,13 +9,12 @@ import (
 	"github.com/demartinom/museum-passport/museums"
 )
 
-// Struct and methods allow for use of Client interfact
 type MockMuseumClient struct {
 	artwork *models.SingleArtwork
 	err     error
 }
 
-func (m *MockMuseumClient) ArtworkbyID(id int) (*models.SingleArtwork, error) {
+func (m *MockMuseumClient) ArtworkByID(id int) (*models.SingleArtwork, error) {
 	if m.err != nil {
 		return nil, m.err
 	}
@@ -26,21 +25,25 @@ func (m *MockMuseumClient) GetMuseumName() string {
 	return "Mock Museum"
 }
 
+func (m *MockMuseumClient) GeneralSearch(string, int) (*museums.SearchResult, error) {
+	return &museums.SearchResult{}, nil
+}
+
 func (m *MockMuseumClient) Search(params museums.SearchParams, _ int) (*museums.SearchResult, error) {
 	if m.err != nil {
 		return nil, m.err
 	}
-	// Return mock IDs for testing
-	return &museums.SearchResult{Art: []*models.SingleArtwork{{
-		ID:           "met-205423",
-		ArtworkTitle: "Dog kennel",
-		ArtistName:   "Claude I Sené",
-		DateCreated:  "ca. 1775–80",
-		ArtMedium:    "Gilded beech and pine; silk and velvet",
-		ImageLarge:   "https://images.metmuseum.org/CRDImages/es/original/DT241800.jpg",
-		ImageSmall:   "https://images.metmuseum.org/CRDImages/es/web-large/DT241800.jpg",
-		Museum:       "The Metropolitan Museum of Art",
-	},
+	return &museums.SearchResult{Art: []*models.SingleArtwork{
+		{
+			ID:           "met-205423",
+			ArtworkTitle: "Dog kennel",
+			ArtistName:   "Claude I Sené",
+			DateCreated:  "ca. 1775–80",
+			ArtMedium:    "Gilded beech and pine; silk and velvet",
+			ImageLarge:   "https://images.metmuseum.org/CRDImages/es/original/DT241800.jpg",
+			ImageSmall:   "https://images.metmuseum.org/CRDImages/es/web-large/DT241800.jpg",
+			Museum:       "The Metropolitan Museum of Art",
+		},
 		{
 			ID:           "met-544519",
 			ArtworkTitle: "Mechanical Dog",
@@ -50,38 +53,41 @@ func (m *MockMuseumClient) Search(params museums.SearchParams, _ int) (*museums.
 			ImageLarge:   "https://images.metmuseum.org/CRDImages/eg/original/0227r2_SEC501K.jpg",
 			ImageSmall:   "https://images.metmuseum.org/CRDImages/eg/web-large/0227r2_SEC501K.jpg",
 			Museum:       "The Metropolitan Museum of Art",
-		}}}, nil
+		},
+	}}, nil
+}
+
+var mockArtwork = &models.SingleArtwork{
+	ID:           "met-123",
+	ArtworkTitle: "Nighthawks",
+	ArtistName:   "Edward Hopper",
+	ArtMedium:    "Oil on canvas",
+	Museum:       "The Metropolitan Museum of Art",
 }
 
 func TestGetArtwork_Success(t *testing.T) {
 	handler := &ArtworkHandler{
 		Clients: map[string]museums.Client{
-			"met": &MockMuseumClient{},
+			"met": &MockMuseumClient{artwork: mockArtwork},
 		},
 	}
-
 	req := httptest.NewRequest("GET", "/api/artwork/met-123", nil)
 	w := httptest.NewRecorder()
-
 	handler.GetArtwork(w, req)
-
 	if w.Code != 200 {
 		t.Errorf("got status %d, want 200", w.Code)
 	}
 }
 
 func TestArtwork_UnknownMuseum(t *testing.T) {
-
 	handler := &ArtworkHandler{
 		Clients: map[string]museums.Client{
-			"guggenheim": &MockMuseumClient{},
+			"guggenheim": &MockMuseumClient{artwork: mockArtwork},
 		},
 	}
 	req := httptest.NewRequest("GET", "/api/artwork/fake-2324", nil)
 	w := httptest.NewRecorder()
-
 	handler.GetArtwork(w, req)
-
 	if w.Code != 404 {
 		t.Errorf("got status %d, want 404", w.Code)
 	}
@@ -90,17 +96,14 @@ func TestArtwork_UnknownMuseum(t *testing.T) {
 func TestGetArtwork_InvalidID(t *testing.T) {
 	handler := &ArtworkHandler{
 		Clients: map[string]museums.Client{
-			"met": &MockMuseumClient{},
+			"met": &MockMuseumClient{artwork: mockArtwork},
 		},
 	}
-
 	req := httptest.NewRequest("GET", "/api/artwork/met-abc", nil)
 	w := httptest.NewRecorder()
-
 	handler.GetArtwork(w, req)
-
 	if w.Code != 400 {
-		t.Errorf("got status %d, want 200", w.Code)
+		t.Errorf("got status %d, want 400", w.Code)
 	}
 }
 
@@ -110,12 +113,9 @@ func TestGetArtwork_NoArtworkFound(t *testing.T) {
 			"met": &MockMuseumClient{err: fmt.Errorf("artwork not found")},
 		},
 	}
-
 	req := httptest.NewRequest("GET", "/api/artwork/met-0000", nil)
 	w := httptest.NewRecorder()
-
 	handler.GetArtwork(w, req)
-
 	if w.Code != 404 {
 		t.Errorf("got status %d, want 404", w.Code)
 	}
