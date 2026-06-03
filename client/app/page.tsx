7ca3c92c9f3428601ca89d0446c5a56185e4ba78
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2026-06-03 16:45:17 -0400

    Add page number to search params and api call

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,17 +7,18 @@ export const dynamic = "force-dynamic";
 export default async function SearchPage({
   searchParams,
 }: {
-  searchParams: Promise<{ q?: string; field?: string }>;
+  searchParams: Promise<{ q?: string; field?: string; page?: string }>;
 }) {
   const params = await searchParams;
   const query = params.q || "";
   const field = params.field || "general";
+  const page = params.page || "1";
 
   let initialResults = [];
 
   if (query) {
     try {
-      const apiUrl = `${process.env.API_URL}/api/search?${field}=${encodeURIComponent(query)}&length=80`;
+      const apiUrl = `${process.env.API_URL}/api/search?${field}=${encodeURIComponent(query)}&length=80&page${page}`;
 
       const res = await fetch(apiUrl, {
         next: { revalidate: 60 }, // Caches the result on the server for 1 minute
