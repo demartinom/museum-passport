db3744dbbf90f16ed01aa7dec96b6bbf11ad6f49
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2026-06-04 14:54:24 -0400

    Change GeneralSearch to take int page number rather than string

--- server/museums/met.go
+++ server/museums/met.go
@@ -168,7 +168,7 @@ func (m *MetClient) SearchRequest(searchIDs []int, resultsLength int) (*SearchRe
 
 // if general is in URL query, searches the api using general search rather than
 // searching by criteria (artist, medium, etc.)
-func (m *MetClient) GeneralSearch(query string, resultsLength int, page string) (*SearchResult, error) {
+func (m *MetClient) GeneralSearch(query string, resultsLength int, pageNumber int) (*SearchResult, error) {
 	queryURL := fmt.Sprintf("%s/search?q=%s", m.BaseURL, url.QueryEscape(query))
 	resp, err := http.Get(queryURL)
 	if err != nil {
@@ -184,7 +184,6 @@ func (m *MetClient) GeneralSearch(query string, resultsLength int, page string) 
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
 		return nil, err
 	}
-	artObjects, err := m.SearchRequest(result.ObjectIDs, resultsLength)
 	if err != nil {
 		return nil, err
 	}
--- server/museums/museums.go
+++ server/museums/museums.go
@@ -7,7 +7,7 @@ type Client interface {
 	GetMuseumName() string
 	ArtworkByID(int) (*models.SingleArtwork, error)
 	Search(SearchParams, int) (*SearchResult, error)
-	GeneralSearch(string, int, string) (*SearchResult, error)
+	GeneralSearch(string, int, int) (*SearchResult, error)
 }
 
 // Used for translating search parameters from url for APIs
