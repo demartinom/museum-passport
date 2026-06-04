925318e9cec08bec39e6a2eeae13f2e7d9b084ab
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2026-06-04 14:59:13 -0400

    Change pageNumber arg for consistency

--- server/museums/artic.go
+++ server/museums/artic.go
@@ -124,8 +124,8 @@ func (a *ArticClient) ArtworkByID(id int) (*models.SingleArtwork, error) {
 	return &normalized, nil
 }
 
-func (a *ArticClient) GeneralSearch(query string, resultsLength int, page int) (*SearchResult, error) {
-	queryURL := fmt.Sprintf("%s/search?q=%s&limit=%d&page=%s&fields=id,title,artist_title,image_id,medium_display,date_start,is_public_domain", a.BaseURL, url.QueryEscape(query), resultsLength, page)
+func (a *ArticClient) GeneralSearch(query string, resultsLength int, pageNumber int) (*SearchResult, error) {
+	queryURL := fmt.Sprintf("%s/search?q=%s&limit=%d&page=%s&fields=id,title,artist_title,image_id,medium_display,date_start,is_public_domain", a.BaseURL, url.QueryEscape(query), resultsLength, pageNumber)
 
 	resp, err := http.Get(queryURL)
 	if err != nil {
