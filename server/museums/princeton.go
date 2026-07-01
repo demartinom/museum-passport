c24e6572814ebe5025aec28558619cb8c8c26658
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2026-07-01 16:13:43 -0400

    Create method for normalizing artwork and saving in cache

--- server/museums/princeton.go
+++ server/museums/princeton.go
@@ -1,6 +1,11 @@
 package museums
 
-import "github.com/demartinom/museum-passport/cache"
+import (
+	"fmt"
+
+	"github.com/demartinom/museum-passport/cache"
+	"github.com/demartinom/museum-passport/models"
+)
 
 // Client for handling calls to the Princeton API
 type PrincetonClient struct {
@@ -26,4 +31,20 @@ func NewPrincetonClient(cache *cache.Cache) *PrincetonClient {
 // Allows for Princeton client to fall under museum interface
 func (p *PrincetonClient) GetMuseumName() string {
 	return "Princeton University Art Museum"
+}
+
+// Takes Object API response store in PrincetonSingleArtwork
+// Normalizes response into the models.Artwork struct and saves in cache
+func (p *PrincetonClient) NormalizeArtwork(receivedArt PrincetonSingleArtwork) models.SingleArtwork {
+	normalized := models.SingleArtwork{
+		ID:           fmt.Sprintf("princeton-%d", receivedArt.ID),
+		ArtworkTitle: receivedArt.Title,
+		ArtistName:   receivedArt.Artist,
+		ArtMedium:    receivedArt.Medium,
+		ImageLarge:   fmt.Sprintf("%s/full/max/0/default.jpg", receivedArt.PrimaryImage),
+		ImageSmall:   fmt.Sprintf("%s/full/800,/0/default.jpg", receivedArt.PrimaryImage),
+		Museum:       p.GetMuseumName(),
+	}
+	p.Cache.SetArtwork(normalized.ID, normalized)
+	return normalized
 }
