2c00bfd5a6e252ab032b533fa1faf4a307c2cdf5
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2026-07-01 10:48:21 -0400

    Create method to update AOTD daily

--- server/handlers/aotd.go
+++ server/handlers/aotd.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/demartinom/museum-passport/cache"
 )
@@ -14,3 +16,21 @@ func NewAOTDHandler(c *cache.Cache) *AOTDHandler {
 	return &AOTDHandler{Cache: c}
 }
 
+func (a AOTDHandler) UpdateAOTD(w http.ResponseWriter, r *http.Request) {
+	secretToken := r.Header.Get("Authorization")
+	if secretToken != os.Getenv("AOTD_PASS") {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		log.Println("DEBUG: Token mismatch!") // <-- Add this too
+		return
+	}
+
+	_ = a.Cache.RemoveOldAOTD()
+
+	winner, err := a.Cache.SetAOTD()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Successfully rotated AOTD to: " + winner))
+}
