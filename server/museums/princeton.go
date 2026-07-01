e6253cbfe490d25724cc03a19156c754b3ed4565
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2026-07-01 10:56:28 -0400

    Create function to start up new Princeton client

--- server/museums/princeton.go
+++ server/museums/princeton.go
@@ -6,3 +6,7 @@ type PrincetonClient struct {
 	BaseURL string
 	Cache   *cache.Cache
 }
+
+func NewPrincetonClient(cache *cache.Cache) *PrincetonClient {
+	return &PrincetonClient{BaseURL: "https://data.artmuseum.princeton.edu/objects/32221", Cache: cache}
+}
