4b2f57de850a4ac7c38db2f7daae2f422232ad4f
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2026-07-09 16:10:29 -0400

    Split main page into home page and search page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,47 +1,20 @@
 import { fraunces } from "../lib/fonts";
-import Link from "next/link";
-import { SearchContent } from "@/components/searchContent";
-import { SearchResult } from "@/types/search";
 import AOTD from "@/components/aotd";
+import { HomeSearchBar } from "@/components/homeSearchBar";
 
-export const dynamic = "force-dynamic";
-
-export default async function SearchPage({
-  searchParams,
-}: {
-  searchParams: Promise<{ q?: string; field?: string; page?: string }>;
-}) {
-  const params = await searchParams;
-  const query = params.q || "";
-  const field = params.field || "general";
-  const page = params.page || "1";
-
-  let initialResults: SearchResult = { totalPages: 0, results: [] };
-
-  if (query) {
-    try {
-      const apiUrl = `${process.env.API_URL}/api/search?${field}=${encodeURIComponent(query)}&length=80&page=${page}`;
-
-      const res = await fetch(apiUrl, {
-        next: { revalidate: 60 }, // Caches the result on the server for 1 minute
-      });
-
-      initialResults = await res.json();
-    } catch (err) {
-      console.error("Server-side fetch error:", err);
-    }
-  }
+export default function HomePage() {
   return (
     <div>
-      <Link href="/" className="flex justify-center">
-        <h1
-          className={`my-6 ${fraunces.className} text-7xl font-bold text-stone-900`}
-        >
-          Museum Passport
-        </h1>
-      </Link>
+      <h1
+        className={`my-6 text-center ${fraunces.className} text-7xl font-bold text-stone-900`}
+      >
+        Museum Passport
+      </h1>
 
-      <SearchContent searchResult={initialResults.results || []} />
+      <div className="px-6">
+        <HomeSearchBar />
+      </div>
+
       <AOTD />
     </div>
   );
