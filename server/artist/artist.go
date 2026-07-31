064f29a846058c4758dbc6d60d928b3999405e44
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2026-07-31 15:27:54 -0400

    Create artist struct

--- server/artist/artist.go
+++ server/artist/artist.go
@@ -1,3 +1,16 @@
 package artist
 type ArtistClient struct {
 }
+type Artist struct {
+	Titles []struct {
+		Name string `json:"normalized"`
+	} `json:"titles"`
+	Blurb string `json:"extract"`
+	Image []struct {
+		ImageURL string `json:"source"`
+		Width    int    `json:"width"`
+		Height   int    `json:"height"`
+	} `json:"originalimage"`
+	Description string `json:"description"`
+}
+
