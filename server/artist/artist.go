0cd2a2243a310a5dc92fb092f9e268701cf8fe93
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2026-07-31 15:29:16 -0400

    Begin implementing search for correct artist page title

--- server/artist/artist.go
+++ server/artist/artist.go
@@ -1,6 +1,15 @@
 package artist
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
 type ArtistClient struct {
 }
+
 type Artist struct {
 	Titles []struct {
 		Name string `json:"normalized"`
@@ -14,7 +23,35 @@ type Artist struct {
 	Description string `json:"description"`
 }
 
+type SearchCandidate struct {
+	Title  string `json:"title"`
+	PageID int    `json:"pageid"`
+}
+
+type CandidateSearchResponse struct {
+	Query struct {
+		Search []SearchCandidate `json:"search"`
+	} `json:"query"`
+}
+
 func (a *ArtistClient) NewArtistClient() *ArtistClient {
 	return &ArtistClient{}
 }
 
+func (a *ArtistClient) FindTitle(query string) (string, error) {
+	encoded := url.QueryEscape(query)
+	queryUrl := fmt.Sprintf("https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=%s&srlimit=3&format=json&origin=*", encoded)
+
+	resp, err := http.Get(queryUrl)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+	var result CandidateSearchResponse
+
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return "", err
+	}
+	return result.Query.Search[0].Title, nil
+}
