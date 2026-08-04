682403b78d6ee7e3e768706cc43a9b9c55d6b83d
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2026-08-04 14:55:02 -0400

    Call Find artist in artist client

--- server/handlers/artist.go
+++ server/handlers/artist.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "github.com/demartinom/museum-passport/cache"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/demartinom/museum-passport/artist"
+	"github.com/demartinom/museum-passport/cache"
+)
 
 type ArtistHandler struct {
 	ArtistClient *artist.ArtistClient
@@ -11,4 +18,15 @@ func NewArtistHandler(a *artist.ArtistClient, c *cache.Cache) *ArtistHandler {
 	return &ArtistHandler{ArtistClient: a, Cache: c}
 }
 
+func (a *ArtistHandler) GetArtist(w http.ResponseWriter, r *http.Request) {
+	// "Monet" placeholder test
+	artist, err := a.ArtistClient.FindArtist("Monet")
+	if err != nil {
+		log.Printf("FindArtist error: %v", err) // TEMP debug
+		http.Error(w, "No artist found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(artist)
 }
